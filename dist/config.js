21162e7d21b22fa1208bbca37824ca190a13df5f
xLazure <xlazure.dev@gmail.com>
2025-06-22 19:17:12 +0200

    add config

---
+++ dist/config.js
@@ -0,0 +1,49 @@
+// Constants
+const MARKET = "PL";
+const COUNTRY_CURRENCY = "zł";
+const AVON_API_URL = "https://apim-eu.api-prod.aws.avon.com";
+
+const TRANSLATIONS = {
+  addToCart: "DODAJ",
+  chooseVariant: "Wybierz wariant",
+  shade: "Odcień",
+  set: "Zestaw",
+};
+
+// Dev variable
+// const campNr = "202506"; // Example campaign number
+
+const getProductUrlApi = (fsc) =>
+  `${AVON_API_URL}/v1/${MARKET}/${MARKET}/product/fscnr/${fsc}?cmpgnId=${campNr}`;
+
+// Base configuration
+const BASE_CONFIG = {
+  TEST_MODE: true,
+  allowedAccounts: ["16806573", "16386183"],
+  userId: getAccNr(),
+  MARKET,
+  COUNTRY_CURRENCY,
+  LOGGER_SERVICE_URL: `v1/${MARKET}/${MARKET}/cm/logger`,
+  AVON_API_URL,
+  getProductUrlApi: (fsc) =>
+    `${AVON_API_URL}/v1/${MARKET}/${MARKET}/product/fscnr/${fsc}?cmpgnId=${campNr}`,
+};
+
+// Feature configuration
+const FEATURE_CONFIG = {
+  bestsellersCarousel: {
+    targetSelector: ".lat-prd.v2",
+    apiUrl: "https://prod-reco.azure-api.net/pl-best/score",
+    apiToken:
+      "9RzyqGwlEtZ6G9r6qzHe3w1NzB1giqio7Dq4yz83EVXzaoaQdDAWJQQJ99BDAAAAAAAAAAAAINFRAZML1lu2",
+    enableLogging: true,
+    responseBody: {
+      RECO_TYPE: "bestsellers",
+    },
+    method: "POST",
+    maxProducts: 12,
+  },
+};
+
+window.RECO_BASE_CONFIG = BASE_CONFIG;
+window.RECO_FEATURE_CONFIG = FEATURE_CONFIG;
